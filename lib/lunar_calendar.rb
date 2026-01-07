515086c3b0d006107a047787c4b9e8d16cb600d0
Llinos Evans <36418502+ShiraTheMogul@users.noreply.github.com>
2026-01-07 15:40:42 +0000

    修复干纸错别字
    已 -> 巳
    西 -> 酉

--- lib/lunar_calendar.rb
+++ lib/lunar_calendar.rb
@@ -13,7 +13,7 @@ module LunarCalendar
   HEAVENLY_STEMS = %w[甲 乙 丙 丁 戊 己 庚 辛 壬 癸].freeze
 
   # @since 0.1.0
-  EARTHLY_STEMS = %w[子 丑 寅 卯 辰 已 午 未 申 西 戌 亥].freeze
+  EARTHLY_STEMS = %w[子 丑 寅 卯 辰 巳 午 未 申 酉 戌 亥].freeze
 
   # Convert Solar to Lunar
   #
